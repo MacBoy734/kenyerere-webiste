8d6d81a6265d51335942e9ca471e4acb64c1e542
Bibilical Baby <bibilicalbaby@yahoo.com>
2025-02-15 00:13:29 +0300

    made changes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+
+const mobileMenu = document.querySelector('.mobile-menu')
+document.getElementById('openMenu').addEventListener('click', () => {
+    mobileMenu.classList.remove('hidden')
+})
+document.getElementById('closeMenu').addEventListener('click', () => {
+    mobileMenu.classList.add('hidden')
+})
 document.getElementById('year').innerText = new Date().getFullYear()
